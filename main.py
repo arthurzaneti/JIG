c22f54279ab2953ad0f703380184fa74ab916cd4
arthurzaneti <125282279+arthurzaneti@users.noreply.github.com>
2023-02-13 13:44:54 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,20 +1,34 @@
-from puzzle import Puzzle
+from puzzle import Puzzle, get_factors
 
 try:
 
     pieces_advertised = int(input("Enter the number of supposed pieces: "))
 
-    # x = input("Enter the horizontal size of the jigsaw image: ")
-    # y = input("Enter the vertical size of the jigsaw image: ")
+    x = float(input("Enter the horizontal size of the jigsaw image: "))
+    y = float(input("Enter the vertical size of the jigsaw image: "))
 
-    # picture_ratio = lambda x, y: x / y if x > y else y / x
-    picture_ratio = 16 / 9
+    if x > y:
+        picture_ratio = x / y
+    else:
+        picture_ratio = y / x
 
     puzzle = Puzzle(pieces_advertised, picture_ratio)
-    # puzzle.get_factors()
-    print(puzzle.possible_grid_sizes())
 
+    puzzle.possible_grids()
+    puzzle.filter_grids()
+    puzzle.piece_ratio()
+    puzzle.decide_best_grid()
 
+    print("With a {} piece puzzle, JIG thinks it is likely that it actually has {} pieces. \n"
+          "This is the result of a grid {} by {}, \n"
+          "witch results in piece ratios of {} and a final score of {}".format(puzzle.pieces_advertised,
+                                                                               puzzle.best_grid[2],
+                                                                               puzzle.best_grid[0], puzzle.best_grid[1],
+                                                                               puzzle.best_grid[3],
+                                                                               puzzle.best_grid[4]))
+except ValueError:
+    print("JIG only accepts numbers...")
 
-except Exception:
-    pass
+except ZeroDivisionError:
+    print("You can´t have a 0 sided image...")
+
--- puzzle.py
+++ puzzle.py
@@ -1,38 +1,64 @@
 class Puzzle:
     def __init__(self, pieces_advertised, picture_ratio):
         self.pieces_advertised = pieces_advertised
-        self.picture_ratio = picture_ratio
-        self.piece_ratio = None
+        self.picture_ratio = picture_ratio  # don't think setting all these values to None is right, maybe there is a better way
+        self.grids = None
+        self.best_grid = None
 
-    def possible_grid_sizes(self):
-        grid_sizes = []
+    def possible_grids(self):
+        grids = []
         for i in range(1, self.pieces_advertised):
             factors = get_factors(i)
-            print(i)
 
-            for i2 in factors:
+            for i2 in factors:  # messy
                 for i3 in factors:
-                    if i2 < i3:
-                        if abs(i3 / i2 - self.picture_ratio) <= 0.1:
-                            grid_sizes.append((i2, i3))
-                            print(abs(i3 / i2 - self.picture_ratio))
-                        else:
-                            if abs(i2 / i3 - self.picture_ratio) <= 0.1:
-                                grid_sizes.append((i2, i3))
-                                print(abs(i3 / i2 - self.picture_ratio))
+                    if i2 < i3:  # checking to guarantee the ratio is bigger than one
+                        if abs(i3 / i2 - self.picture_ratio) <= 0.2:  # arbitrary decision for 20% to be the maximal difference here
 
-        return grid_sizes
+                            if grids.count([i3, i2]) == 0:  # checking to see if the element is already in the list
+                                grids.append([i3, i2])
+
+                    else:
+                        if abs(i2 / i3 - self.picture_ratio) <= 0.2:
+                            if grids.count([i2, i3]) == 0:
+                                grids.append([i2, i3])
+
+        self.grids = grids
 
     def piece_ratio(self):
-        pass
+        for i in self.grids:
+            ratio = i[0] / i[1] / self.picture_ratio
+            i.append(ratio)
 
+    def filter_grids(self):
+        filtered_grids = []
 
+        for grid in self.grids:
+
+            #  arbitrary decision to set the cap to exceeding pieces ate 10% the total advertised, might change later
+            if 0 <= (grid[0] * grid[1] - self.pieces_advertised) <= (self.pieces_advertised / 10):
+                filtered_grids.append([grid[0], grid[1], grid[0] * grid[1]])
+
+        self.grids = filtered_grids
+
+    def decide_best_grid(self):
+
+        for grid in self.grids:
+            exceeding_pieces = grid[2] - self.pieces_advertised
+            score = abs(1 - grid[3]) * 100 + exceeding_pieces
+            grid.append(score)
+
+        best_grid = [0, 0, 0, 0, 100000000]  # find a way out of this later
+        for grid in self.grids:
+            if grid[4] < best_grid[4]:
+                best_grid = grid
+        self.best_grid = best_grid
 
 
 def get_factors(n):
     factors = []
 
-    for i in range(1, n):
+    for i in range(1, n + 1):
         if n % i == 0:
             factors.append(i)
 
